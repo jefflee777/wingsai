d6bb68fdab68889c83f0cba282236968e37f3261
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-08-16 13:15:33 +0530

    font and theme updated

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      
+    <div className="text-4xl text-center py-64">
+      hello 
     </div>
   );
 }
