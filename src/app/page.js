5799de4bc2ffad8bc242e2185384e9d0466fe45a
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-08-18 22:56:31 +0530

    navbar linked with the index page

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import CTASection from "@/components/cta";
 import EcosystemBenefits from "@/components/EcosystemBenefits";
 import Footer from "@/components/Footer";
+import Navbar from "@/components/Header";
 import HeroSection from "@/components/Hero";
 import HowItWorks from "@/components/HowItWorks";
 import KeyFeatures from "@/components/KeyFeatures";
@@ -10,6 +11,7 @@ import Tokenomics from "@/components/Tokenomics";
 export default function Home() {
   return (
     <div>
+      <Navbar/>
       <HeroSection/>
       <HowItWorks/>
       <KeyFeatures/>
