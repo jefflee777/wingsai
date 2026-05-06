43a17f201fee24719ab9060d4ce4beee385241a2
Sourav Chhimpa <souravchhimpa1@gmail.com>
2026-05-06 13:41:27 +0530

    chore: comment out Tokenomics and Roadmap sections in landing page

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,8 @@ import About from "@/components/landing/About";
 import Features from "@/components/landing/Features";
 import HowItWorks from "@/components/landing/HowItWorks";
 import EcosystemBenefits from "@/components/landing/EcosystemBenefits";
-import Tokenomics from "@/components/landing/Tokenomics";
-import Roadmap from "@/components/landing/Roadmap";
+// import Tokenomics from "@/components/landing/Tokenomics";
+// import Roadmap from "@/components/landing/Roadmap";
 import CTA from "@/components/landing/CTA";
 import Footer from "@/components/landing/Footer";
 
@@ -19,8 +19,8 @@ export default function Home() {
         <Features />
         <HowItWorks />
         <EcosystemBenefits />
-        <Tokenomics />
-        <Roadmap />
+        {/* <Tokenomics />
+        <Roadmap /> */}
         <CTA />
       </main>
       <Footer />
